9f13d78412593e5e4ab87401702215ee308632e3
dinesh <Dinesh@example.com>
2024-11-25 11:56:48 +0530

    final

--- client/src/redux/user/userslice.js
+++ client/src/redux/user/userslice.js
@@ -33,10 +33,35 @@ const userSlice = createSlice({
         updateUserFailure:(state,action)=>{
             state.loading = false;
             state.error = action.payload
-        }
+        },deleteUserStart: (state) => {
+            state.loading = true;
+          },
+          deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+          },
+          deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+          },
+          signOutUserStart: (state) => {
+            state.loading = true;
+          },
+          signOutUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+          },
+          signOutUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+          },
     }
 })
 
-export const {signInStart , signInSuccess , signInFailure,  updateUserFailure,  updateUserSuccess ,  updateUserStart} = userSlice.actions;
+export const {signInStart , signInSuccess , signInFailure,  updateUserFailure,  updateUserSuccess ,  updateUserStart,
+    deleteUserFailure , deleteUserSuccess , deleteUserStart, signOutUserFailure,signOutUserSuccess,signOutstart
+} = userSlice.actions;
 
 export default userSlice.reducer
