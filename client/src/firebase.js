d64840227c15aa31643007620d12582d3d6aef2c
dinesh <Dinesh@example.com>
2024-11-28 11:08:43 +0530

    overall project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/supabase.js
+import { createClient } from '@supabase/supabase-js';
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-5cc03.firebaseapp.com",
-  projectId: "mern-estate-5cc03",
-  storageBucket: "mern-estate-5cc03.firebasestorage.app",
-  messagingSenderId: "560064948725",
-  appId: "1:560064948725:web:19aab1635ccea2d60ed8bc"
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+// Supabase configuration
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;  // Your Supabase URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;  // Your Supabase Anon key
+export const supabase = createClient(supabaseUrl, supabaseKey);
