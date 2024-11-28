f60ab7a90c6a8fa782cd064d291284613cb535e8
dinesh <Dinesh@example.com>
2024-11-28 11:16:30 +0530

    error cleared

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,17 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Supabase configuration
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;  // Your Supabase URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;  // Your Supabase Anon key
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { supabase } from '../supabase';  // Import the Supabase client
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
